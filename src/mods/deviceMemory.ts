68435d54b132bf17b48ff9690bd4e793bdfa651a
Bruno Garcia <b@aduros.com>
2023-11-16 13:14:02 -0300

    Fix tests.

--- src/mods/deviceMemory.ts
+++ src/mods/deviceMemory.ts
@@ -1,33 +1,19 @@
 import { Scope, modifyGetter } from '.';
 
-declare global {
-  interface Navigator {
-    deviceMemory?: number;
-  }
-
-  interface MemoryInfo {
-    totalJSHeapSize: number;
-    usedJSHeapSize: number;
-    jsHeapSizeLimit: number;
-  }
-  var MemoryInfo: {
-      prototype: MemoryInfo;
-      new(): MemoryInfo;
-  };
-}
-
 export function modifyDeviceMemory (scope: Scope) {
-  modifyGetter(scope.Navigator.prototype, 'deviceMemory', ({originalValue, random}) => {
-    if (typeof originalValue === 'number') {
-      return 2 ** random.nextIntBetween(0, 3);
-    } else {
-      return originalValue;
-    }
-  });
+  // Chrome only
+  if ('deviceMemory' in scope.Navigator.prototype) {
+    modifyGetter(scope.Navigator.prototype, 'deviceMemory', ({originalValue, random}) => {
+      if (typeof originalValue === 'number') {
+        return 2 ** random.nextIntBetween(0, 3);
+      } else {
+        return originalValue;
+      }
+    });
+  }
 
-  // modifyGetter((scope.performance as any).memory, 'totalJSHeapSize', () => 0);
-  // modifyGetter((scope.performance as any).memory, 'usedJSHeapSize', () => 0);
-  // modifyGetter((scope.performance as any).memory, 'jsHeapSizeLimit', () => 0);
-
-  modifyGetter((scope.Performance.prototype as any), 'memory', () => undefined);
+  // Chrome only
+  if ('memory' in scope.Performance.prototype) {
+    modifyGetter(scope.Performance.prototype, 'memory', () => undefined);
+  }
 }
